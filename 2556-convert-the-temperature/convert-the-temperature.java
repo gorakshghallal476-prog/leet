47e00b1c60c0e4b61a7ba882f4d3d47937e2b372
goraksh ghallal <gorakshghallal476@gmail.com>
2026-05-16 22:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (22.71%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double array[]=new double[2];
+        array[0]=celsius+273.15;
+        array[1]=celsius*1.80+32;
+        return array;
+        
+    }
+}
